4e36506a5b3b6bf55acd20c32a7902ecc68ae96e
Jakub Widurek <jakubwidurek@gmail.com>
2023-07-19 23:34:16 +0200

    init ts env and create first Block

---
+++ src/block.ts
@@ -0,0 +1,24 @@
+interface Block {
+  timestamp: string;
+  data: string;
+  hash: string;
+  lastHash: string;
+}
+
+class Block {
+  constructor({ timestamp, data, hash, lastHash }: Block) {
+    this.timestamp = timestamp;
+    this.data = data;
+    this.hash = hash;
+    this.lastHash = lastHash;
+  }
+}
+
+console.log(
+  new Block({
+    timestamp: '01/01/01',
+    data: 'one',
+    hash: 'fo-hash',
+    lastHash: 'last-fo-hash',
+  })
+);
---
+++ tests/index.test.ts
@@ -0,0 +1,5 @@
+// describe('testing "add" function', () => {
+//   test('add 4+3 to be 8', () => {
+//     expect(add(4, 3)).toBe(7);
+//   });
+// });
